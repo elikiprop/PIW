fbacc088737bf46ee615ffbb2925503ca7355ea8
const_celine <127382092+juice2227@users.noreply.github.com>
2024-05-16 22:45:37 +0000

    changes

--- src/components/Home/Statsblock.jsx
+++ src/components/Home/Statsblock.jsx
@@ -1,51 +1,53 @@
-import React from 'react'
+import React from "react";
 // import "../../assets/scss/theme.scss"
 // import "../../assets/css/theme.css"
 const Statsblock = () => {
   return (
     <>
-        <div className="stats-block theme-bg-primary text-white py-4 text-center">
-            <div className="container">
-                <div className="row">
-                    <div className="col-6 col-md-3">
-                        <div className="item">
-                            <div className="number">3000+</div>
-                            <div className="unit">Delegates</div>
-                        </div>
-                        {/* <!--//item--> */}
-                    </div>
-                    {/* <!--//col--> */}
-                    <div className="col-6 col-md-3">
-                        <div className="item">
-                            <div className="number">5</div>
-                            <div className="unit">Days</div>
-                        </div>
-                        {/* <!--//item--> */}
-                    </div>
-                    {/* <!--//col--> */}
-                    <div className="col-6 col-md-3">
-                        <div class="item">
-                            <div className="number">50+</div>
-                            <div className="unit">Sessions</div>
-                        </div>
-                        {/* <!--//item--> */}
-                    </div>
-                    {/* <!--//col--> */}
-                    <div className="col-6 col-md-3">
-                        <div className="item">
-                            <div className="number">10+</div>
-                            <div className="unit">Exhibitors</div>
-                        </div>
-                        {/* <!--//item--> */}
-                    </div>
-                    {/* <!--//col--> */}
-                </div>
+      <div className="stats-block theme-bg-primary text-white py-4 text-center">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 col-md-3">
+              <div className="item">
+                <div className="number">3000+</div>
+                <div className="unit">Delegates</div>
+              </div>
+              {/* <!--//item--> */}
             </div>
-            {/* <!--//container--> */}
-        </div>
-        {/* <!--//stats-block--> */}
-  </>
-  )
-}
+            {/* <!--//col--> */}
+            <div className="col-6 col-md-3">
+              <div className="item">
+                <div className="number">5</div>
+                <div className="unit">Days</div>
+              </div>
+              {/* <!--//item--> */}
+            </div>
 
-export default Statsblock
+            {/* <!--//col--> */}
+            <div className="col-6 col-md-3">
+              <div class="item">
+                <div className="number">12+</div>
+                <div className="unit">Engaging plenary Sessions</div>
+              </div>
+              {/* <!--//item--> */}
+            </div>
+            {/* <!--//col--> */}
+            <div className="col-6 col-md-3">
+              <div className="item">
+                <div className="number">53+</div>
+                <div className="unit">Diverse</div>
+              </div>
+              {/* <!--//item--> */}
+            </div>
+
+            {/* <!--//col--> */}
+          </div>
+        </div>
+        {/* <!--//container--> */}
+      </div>
+      {/* <!--//stats-block--> */}
+    </>
+  );
+};
+
+export default Statsblock;
